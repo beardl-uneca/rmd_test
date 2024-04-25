2ea5c926ca4be42eb761ce7075f56682160b4e98
Beardsmore <liam.beardsmore@ons.gov.uk>
2024-04-25 15:42:18 +0100

    initial push

---
+++ render.R
@@ -0,0 +1,3 @@
+rmarkdown::render("D:/repos/Rmd_test/check.Rmd", output_format = "word_document",
+                  output_file = "D:/repos/Rmd_test/check.docx",
+                  output_options = list(reference_docx = "D:/repos/Rmd_test/Report_Style.docx"))
